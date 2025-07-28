10e9772ed33a6ea9c112a880c50081240468a2eb
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-07-28 14:51:52 +0530

    Wallet Models Added

---
+++ Carple.Domain/Enities/Wallet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carple.Domain.Enities
+{
+    public class Wallet
+    {
+        [Key]
+        public int WalletId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        //[ForeignKey("UserId")]
+        //public User User { get; set; }  
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Balance { get; set; }
+
+        // Navigation property
+        public ICollection<WalletTransaction> Transactions { get; set; }
+    }
+}
---
+++ Carple.Domain/Enities/WalletTransaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carple.Domain.Enities
+{
+    public class WalletTransaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Required]
+        public int WalletId { get; set; }
+
+        [ForeignKey("WalletId")]
+        public Wallet Wallet { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Amount { get; set; }
+
+        [MaxLength(20)]
+        public string TransactionType { get; set; }  // e.g., Credit or Debit
+
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+    }
+}
